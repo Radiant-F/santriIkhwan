f486e56af17601a8c87f2d256ac3a3c406314e39
Radiant <you@example.com>
2020-09-18 21:33:21 +0700

    menambah dua files

---
+++ script.js
@@ -0,0 +1,7 @@
+var santriIkhwan = ['Sidik', 'Rahmad', 'Syofyan', 'Aldi', 'Kardi'];
+
+const prabu = [Lampung, Prabumulih, ...other] = santriIkhwan
+
+console.log(Lampung)
+console.log(Prabumulih)
+console.log(other)
